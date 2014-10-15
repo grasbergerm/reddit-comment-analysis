91e4ddc7f6888ac109d1bfd659c087a515de7022
Matthew <grasbergerm@gmail.com>
2014-10-15 12:02:02 -0400

    V 0.2b
    Now counting replies and replies to replies

--- comment_analysis.py
+++ comment_analysis.py
@@ -1,16 +1,32 @@
 import praw
+
+# Used to count recursive calls to checkComments
+class Counter(object) :
+    def __init__(self, fun) :
+        self._fun = fun
+        self.counter=0
+    def __call__(self,*args, **kwargs) :
+        self.counter += 1
+        return self._fun(*args, **kwargs)
+
+# Used to recursively grab replies to comments
 def checkComments(comment,com_count):
     for comment in comment.replies:
-        com_count += 1
         print "\t"*com_count,
         print "%s" % comment
-        checkComments(comment,com_count)
+        checkComments(comment,com_count+1)
 
-user_agent = ("comment analysis by grasbergerm")
+checkComments = Counter(checkComments)
+
+user_agent = ("comment analysis by grasbergerm - https://github.com/grasbergerm/reddit-comment-analysis")
 r = praw.Reddit(user_agent=user_agent)
 subreddit = r.get_subreddit('drunk')
-post = r.get_submission(submission_id='2j72h3')
+post = r.get_submission(submission_id='2j92q0')
+orig_comment = 0
 for comment in post.comments:
-        print "-%s\n" % comment
-        com_count = 0
-        checkComments(comment,com_count)
+    orig_comment += 1
+    print "-%s\n" % comment
+    com_count = 0
+    print checkComments(comment,com_count)
+    print checkComments.counter - orig_comment
+print orig_comment
