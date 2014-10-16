6a11be964f606782f249f28a0176dae9b6521b87
Matthew <grasbergerm@gmail.com>
2014-10-16 01:42:31 -0400

    Multi-post
    Updated to read the hot ten of any subreddit.

--- comment_analysis.py
+++ comment_analysis.py
@@ -22,20 +22,24 @@ checkComments = Counter(checkComments)
 
 user_agent = ("comment analysis by grasbergerm - https://github.com/grasbergerm/reddit-comment-analysis")
 r = praw.Reddit(user_agent=user_agent)
-subreddit = r.get_subreddit('drunk')
-submission = r.get_submission(submission_id='2j92q0')
-submission.replace_more_comments(limit=None, threshold=0)
-#all_comments = submission.comments
-orig_comment = 0
-for comment in submission.comments:
-    orig_comment += 1
-    #print "-%s\n" % comment
-    com_count = 0
-    checkComments(comment,com_count)
-print "REPLIES \t",
-print "="*((checkComments.counter - orig_comment)/10)
-print "\t\t",(checkComments.counter - orig_comment)
-print "ORIGINAL\t",
-print "="*((orig_comment)/10)
-print "\t\t",(orig_comment)
+subreddit = r.get_subreddit('jeep')
+# Used for a single submission
+#submission = r.get_submission(submission_id='2j92q0')
+for submission in subreddit.get_hot(limit=10):
+    submission.replace_more_comments(limit=None, threshold=0)
+    #all_comments = submission.comments
+    orig_comment = 0
+    for comment in submission.comments:
+        orig_comment += 1
+        #print "-%s\n" % comment
+        com_count = 0
+        checkComments(comment,com_count)
+    print "%s" % submission
+    print "REPLIES \t",
+    print "="*int(round((checkComments.counter - orig_comment)/6.0))
+    print "\t",(checkComments.counter - orig_comment)
+    print "ORIGINAL\t",
+    print "="*int(round((orig_comment)/6.0))
+    print "\t",(orig_comment)
+    checkComments.counter = 0
 
